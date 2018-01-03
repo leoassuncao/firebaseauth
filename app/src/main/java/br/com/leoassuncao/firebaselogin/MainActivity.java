90d7b1e8a2f363b6ad3145dd5bdfca4ccbef4115
Leonardo Vinicius de Assunção S Filho <leonardo.filho@df.mi>
2018-01-03 16:07:15 -0200

    first commit

---
+++ app/src/main/java/br/com/leoassuncao/firebaselogin/MainActivity.java
@@ -0,0 +1,11 @@
+package br.com.leoassuncao.firebaselogin;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by leonardo.filho on 03/01/2018.
+ */
+
+public class MainActivity extends AppCompatActivity{
+}
