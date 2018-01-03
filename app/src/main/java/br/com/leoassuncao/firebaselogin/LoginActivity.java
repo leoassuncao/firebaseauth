b1269303245202ef5d30d6934cab8a2f70a50b36
Leonardo Vinicius de Assunção S Filho <leonardo.filho@df.mi>
2018-01-03 16:33:11 -0200

    add remember password

--- app/src/main/java/br/com/leoassuncao/firebaselogin/LoginActivity.java
+++ app/src/main/java/br/com/leoassuncao/firebaselogin/LoginActivity.java
@@ -36,11 +36,11 @@ public class LoginActivity extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
 
-        if(auth.getCurrentUser() != null) {
+      /*  if(auth.getCurrentUser() != null) {
             startActivity(new Intent(LoginActivity.this, MainActivity.class));
             finish();
         }
-
+*/
         setContentView(R.layout.activity_login);
 
 
